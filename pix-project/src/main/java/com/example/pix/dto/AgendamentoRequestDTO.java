d43fe605c114f87bb3c5cb28e5dd572e495687e7
Eric Bach <eric.bach@gmail.com>
2025-10-27 14:14:21 -0300

    correcao gradle e toString

--- pix-project/src/main/java/com/example/pix/dto/AgendamentoRequestDTO.java
+++ pix-project/src/main/java/com/example/pix/dto/AgendamentoRequestDTO.java
@@ -1,10 +1,7 @@
 package com.example.pix.dto;
 
 import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
@@ -13,6 +10,7 @@ import java.time.OffsetDateTime;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class AgendamentoRequestDTO {
 
     @NotNull
